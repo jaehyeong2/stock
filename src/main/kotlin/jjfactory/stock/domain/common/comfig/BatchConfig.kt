52c89b28b187f3d1f5da9ee6cf42e21d376e4629
jaehyeong2 <wogud1514@gmail.com>
2023-12-09 14:43:23 +0900

    chore: batch config

---
+++ src/main/kotlin/jjfactory/stock/domain/common/comfig/BatchConfig.kt
@@ -0,0 +1,9 @@
+package jjfactory.stock.domain.common.comfig
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.context.annotation.Configuration
+
+@EnableBatchProcessing
+@Configuration
+class BatchConfig {
+}
