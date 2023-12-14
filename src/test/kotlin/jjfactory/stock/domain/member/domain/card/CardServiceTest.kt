f961157659d9834636b3ce640d3d99ffcbfc5257
jaehyeong <wogud1514@gmail.com>
2023-12-14 22:52:14 +0900

    3. cardRepository save test

--- src/test/kotlin/jjfactory/stock/domain/member/domain/card/CardServiceTest.kt
+++ src/test/kotlin/jjfactory/stock/domain/member/domain/card/CardServiceTest.kt
@@ -1,19 +1,34 @@
 package jjfactory.stock.domain.member.domain.card
 
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 
-class CardServiceTest {
+class CardServiceTest(
+
+){
+    private lateinit var cardRepository: CardRepository
+
+    @BeforeEach
+    fun setUp(){
+        cardRepository = CardRepository()
+    }
+
 
     @Test
     fun createTest() {
-        CardCommand.Create(
+        val card = CardCommand.Create(
             memberId = 2L,
             alias = "alias",
             cardNumber = "cardNumber",
             expireDate = LocalDateTime.now(),
             cvv = "534"
-        )
+        ).toEntity()
+
+        val idx = cardRepository.save(card)
+
+        assertThat(idx).isEqualTo(1)
     }
 
     class CardCommand {
@@ -43,5 +58,15 @@ class CardServiceTest {
         val expireDate: LocalDateTime,
         val cvv: String
     )
+
+    class CardRepository{
+        private var idx: Long = 0
+        private val map: MutableMap<Card, Long> = HashMap()
+
+        fun save(card: Card): Long{
+            map[card] = idx++
+            return idx
+        }
+    }
 }
 
