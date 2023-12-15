c275884717d897a36f55f415e1a86a9e4c11db93
jaehyeong <wogud1514@gmail.com>
2023-12-15 11:59:29 +0900

    feature: base entity 추가

---
+++ src/main/kotlin/jjfactory/stock/domain/common/comfig/JpaConfig.kt
@@ -0,0 +1,9 @@
+package jjfactory.stock.domain.common.comfig
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+}
