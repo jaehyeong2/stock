ee50f2b406c1c0eb682bc8fa858121c919667e1b
jaehyeong <wogud1514@gmail.com>
2023-12-15 11:59:29 +0900

    base entity 추가

---
+++ src/main/kotlin/jjfactory/stock/domain/common/BaseEntity.kt
@@ -0,0 +1,21 @@
+package jjfactory.stock.domain.common
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+open class BaseEntity {
+
+    @CreatedDate
+    var createdAt: LocalDateTime? = null
+        private set
+
+    @LastModifiedDate
+    var modifiedAt: LocalDateTime? = null
+        private set
+}
