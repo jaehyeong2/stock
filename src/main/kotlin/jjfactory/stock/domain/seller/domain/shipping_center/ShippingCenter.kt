891d1a75f2009dc5e74199a0f453c1d135aed4df
jaehyeong2 <wogud1514@gmail.com>
2023-12-16 15:06:46 +0900

    1. shipping center entity 추가

---
+++ src/main/kotlin/jjfactory/stock/domain/seller/domain/shipping_center/ShippingCenter.kt
@@ -0,0 +1,21 @@
+package jjfactory.stock.domain.seller.domain.shipping_center
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class ShippingCenter(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    val sellerId: Long,
+    var name: String,
+    var usable: Boolean = true,
+    var contactNumber: String,
+
+    var zipCode: String,
+    var address: String,
+    var addressDetail: String
+) {
+}
