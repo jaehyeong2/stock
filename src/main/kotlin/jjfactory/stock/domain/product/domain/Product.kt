aedaf134723753b74b132a0ff1c021324b5b6b1d
jaehyeong <wogud1514@gmail.com>
2023-12-16 12:03:59 +0900

    1. product state 추가

--- src/main/kotlin/jjfactory/stock/domain/product/domain/Product.kt
+++ src/main/kotlin/jjfactory/stock/domain/product/domain/Product.kt
@@ -1,20 +1,30 @@
 package jjfactory.stock.domain.product.domain
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import jjfactory.stock.domain.common.BaseEntity
 
 @Entity
 class Product(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id :Long? = null,
+    val id: Long? = null,
     val categoryId: Long,
     val sellerId: Long,
-    var exposeName:  String,
+    var exposeName: String,
     var enrollName: String,
-    var code: String
-): BaseEntity() {
+    val code: String,
+    @Enumerated(EnumType.STRING)
+    var state: ProductState
+) : BaseEntity() {
+
+    fun changePreParing(){
+        state = ProductState.PREPARING
+    }
+
+    fun changeOnSales(){
+        state = ProductState.ON_SALES
+    }
+
+    fun endOfSales(){
+        state = ProductState.END_OF_SALE
+    }
 }
