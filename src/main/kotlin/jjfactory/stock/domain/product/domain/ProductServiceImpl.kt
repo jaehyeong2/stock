cd88c94bb40c4891b485c46887d730ee6ee96a94
jaehyeong <wogud1514@gmail.com>
2023-12-16 12:36:25 +0900

    1. service 인터페이스 생성

--- src/main/kotlin/jjfactory/stock/domain/product/domain/ProductServiceImpl.kt
+++ src/main/kotlin/jjfactory/stock/domain/product/domain/ProductServiceImpl.kt
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class ProductService(
+class ProductServiceImpl(
     private val productReader: ProductReader,
     private val productWriter: ProductWriter,
     private val categoryReader: CategoryReader
-) {
+): ProductService {
     @Transactional(readOnly = true)
-    fun findById(id: Long): ProductInfo {
+    override fun findById(id: Long): ProductInfo {
         return productReader.findById(id)
     }
 
     @Transactional
-    fun save(sellerId: Long, command: ProductCommand.Create, categoryId: Long): Long {
+    override fun save(sellerId: Long, command: ProductCommand.Create, categoryId: Long): Long {
         val category = categoryReader.findById(categoryId)
         val product = productWriter.write(command.toEntity(category.id!!))
         return product.id!!
