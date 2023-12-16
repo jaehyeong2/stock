3a835e149e31b740310d46bbf8c0b9efedcff1bd
jaehyeong2 <wogud1514@gmail.com>
2023-12-16 15:37:19 +0900

    3. shipping center paging query 추가

--- src/main/kotlin/jjfactory/stock/domain/seller/infra/shipping_center/ShippingCenterReaderImpl.kt
+++ src/main/kotlin/jjfactory/stock/domain/seller/infra/shipping_center/ShippingCenterReaderImpl.kt
@@ -1,8 +1,11 @@
 package jjfactory.stock.domain.seller.infra.shipping_center
 
 import jjfactory.stock.domain.seller.domain.shipping_center.ShippingCenter
+import jjfactory.stock.domain.seller.domain.shipping_center.ShippingCenterInfo
 import jjfactory.stock.domain.seller.domain.shipping_center.ShippingCenterReader
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
@@ -12,5 +15,22 @@ class ShippingCenterReaderImpl(
 ) : ShippingCenterReader {
     override fun findById(id: Long): ShippingCenter {
         return shippingCenterRepository.findByIdOrNull(id) ?: throw NotFoundException()
+    }
+
+    override fun findBySellerId(sellerId: Long, pageable: Pageable): Page<ShippingCenterInfo.Main?> {
+        return shippingCenterRepository.findPage(pageable) {
+            selectNew<ShippingCenterInfo.Main>(
+                path(ShippingCenter::id),
+                path(ShippingCenter::name),
+                path(ShippingCenter::contactNumber),
+                path(ShippingCenter::zipCode),
+                path(ShippingCenter::address),
+                path(ShippingCenter::addressDetail),
+            ).from(
+                entity(ShippingCenter::class)
+            ).where(
+                path(ShippingCenter::sellerId).eq(sellerId)
+            )
+        }
     }
 }
